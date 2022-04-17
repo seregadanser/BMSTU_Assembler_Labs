5e2f4d8f622ebe073a839a0b6182848d3af54261
seregadancer <kalashnikov.sergey01@gmail.com>
2022-04-17 21:46:53 +0300

    add out resedent

--- lab7/main.asm
+++ lab7/main.asm
@@ -1,10 +1,12 @@
+.model tiny
+
 Code SEGMENT PARA PUBLIC 'CODE'
     assume CS:Code, DS:Code
     org 100h
     ;jmp init
+    is_init db 33
     old_int_handler dd 0
     local_counter db 0
-    local_factor db ?;задержка
     speed db ?  
 
     Input PROC far
@@ -19,7 +21,7 @@ Code SEGMENT PARA PUBLIC 'CODE'
         call cs:old_int_handler;вызов старого обработчика
 
         mov bl, cs:local_counter
-        cmp bl, cs:local_factor
+        cmp bl, 17 ;задержка
 
         jb exit1
 
@@ -60,8 +62,10 @@ Code SEGMENT PARA PUBLIC 'CODE'
 
     ;START;
     init:
-        cli
-        mov byte ptr local_factor, 17
+        mov dl, 30h
+        mov ah, 02h
+        int 21h
+
         mov byte ptr speed, 1fh
 
         mov al, 0f3h
@@ -72,20 +76,44 @@ Code SEGMENT PARA PUBLIC 'CODE'
         mov al, cs:speed
         out 60h, al
         
-
         mov ax,351ch
         int 21h
+
+        cmp byte ptr es:is_init, 32h
+        je exit_out
+
         mov word ptr old_int_handler, bx
         mov word ptr old_int_handler + 2, es
         
         mov ax,251ch    ; установить вектор прерывания
         mov dx,OFFSET Input
         int 21H
+
+        mov byte ptr is_init, 32h
         
         mov ax,3100h   ; завершиться и остаться резидентным
         mov dx,OFFSET init
-        sti
+
         int 21H
+
+    exit_out:
+        mov dl, 31h
+        mov ah, 02h
+        int 21h
+
+        mov al, 0F3h
+        out 60h, al
+
+        mov al, 0
+        out 60h, al
+
+        mov dx, word ptr es:old_int_handler
+        mov ds, word ptr es:old_int_handler+2
+        mov ax, 251ch
+        int 21h
+
+        mov ax, 4c00h
+        int 21h
 
 Code ENDS
 
